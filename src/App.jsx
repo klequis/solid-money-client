64113780b9a14284f73e39c7786c65392daedb23
klequis <klequis@gmail.com>
2025-04-23 16:44:22 -0700

    tx in store, table populated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from "./App.module.css";
+import { createStore } from "solid-js/store";
+import {
+  createEffect,
+  createRenderEffect,
+  createResource,
+  For,
+} from "solid-js";
+// import { fetchJson } from './api';
+import { fetchJson } from "./api/apiHelpers";
+
+async function fetchTx() {
+  const r = await fetchJson("/api/transactions");
+  return r;
+}
+
+const formatAmount = amount => {
+  return amount.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+}
 
 function App() {
+  const [transactions, { mutate }] = createResource(fetchTx);
+  const [store, setStore] = createStore({
+    txCount: 3,
+  });
+
+  // createEffect(() => {
+  //   console.log('transactions', transactions())
+  // })
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <h1>Solid Money</h1>
+      <p>Number of users: {store.txCount}</p>
+      <table>
+        <For each={transactions()}>
+          {(tx, idx) => (
+            <tr class={idx() % 2 === 0 ? styles.trDark : ""}>
+            {/* <tr class={styles.trDark}> */}
+              <td>{idx}</td>
+              <td>{tx.acctId}</td>
+              <td class={styles.tdDate}>{new Date(tx.date).toLocaleDateString()}</td>
+              <td>{tx.description}</td>
+              <td class={styles.tdAmount}>{formatAmount(tx.amount)}</td>
+            </tr>
+          )}
+        </For>
+      </table>
     </div>
   );
 }
